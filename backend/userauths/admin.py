8b4bd583b89eb5a482f788461830dd1b7e3d9c80
Meraj_Dh <m.dahmardeh16@gmail.com>
2025-12-01 22:54:45 +0330

    feat(userauth):add Auth Logic and API Doc

--- backend/userauths/admin.py
+++ backend/userauths/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from userauths.models import Profile , User
 
-admin.site.register(User)
-admin.site.register(Profile)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['full_name' , "phone" , "email" ]
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['full_name' , "gender" , "date" ]
+    list_filter = ["date"]
+
+admin.site.register(User , UserAdmin)
+admin.site.register(Profile , ProfileAdmin)
 
 # Register your models here.
--- backend/userauths/views.py
+++ backend/userauths/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated , AllowAny
 
-# Create your views here.
+
+from userauths.models import User, Profile
+from userauths.serializer import MyTokenSerializer,RegisterSerializer
+class MyTokenObtainView(TokenObtainPairView):
+    serializer_class  = MyTokenSerializer
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = RegisterSerializer
